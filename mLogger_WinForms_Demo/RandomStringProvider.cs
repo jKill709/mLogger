d189582efc7d1d3f12c0f4dc75836ebb0521dd78
jKill709 <jeremykillinger@gmail.com>
2026-07-18 22:53:06 -0400

    Added RandomStringProvider class

---
+++ mLogger_WinForms_Demo/RandomStringProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mLogger_WinForms_Demo
+{
+    internal class RandomStringProvider
+    {
+        private readonly List<string> _strings = new();
+
+        public void AddString(string str)
+        {
+            _strings.Add(str);
+        }
+
+        public void AddStrings(IEnumerable<string> strings)
+        {
+            _strings.AddRange(strings);
+        }
+
+        public string GetString()
+        {
+            if (_strings.Count == 0)
+                throw new InvalidOperationException("No strings have been added.");
+
+            return _strings[Random.Shared.Next(_strings.Count)];
+        }
+    }
+}
